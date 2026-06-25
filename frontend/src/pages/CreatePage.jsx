45f37e3163fd98a473831e4472eefba238859610
krishivnarainagarwal <krishivagarwal01@GMAIL.COM>
2026-06-26 01:45:44 +0530

    basic frontend setup done

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+const CreatePage = () => {
+  return (
+    <div>
+      <h1>Create a Note</h1>
+    </div>
+  );
+};
+
+export default CreatePage; 
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,9 @@
+const NoteDetailPage = () => {
+  return (
+    <div>
+      <h1>Note Details</h1>
+    </div>
+  );
+};
+
+export default NoteDetailPage; 
